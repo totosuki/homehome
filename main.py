29d7ec0c646461d16390f354730420fa0f333032
toripppppy <lundy11@icloud.com>
2025-03-19 23:02:17 +0900

    空のAPIを作成

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# ほめ言葉を一つ返す
+@app.get("/home")
+def get_home():
+    # TODO DBから褒め言葉を一つ取得して返す処理
+    return {"text": "生きててえらい"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# ほめ言葉を一つ追加する
+@app.post("/home")
+def post_home(req):
+    # TODO リクエストからDBに褒め言葉を保存する処理
+    return {"req": req}
