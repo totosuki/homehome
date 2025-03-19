9760ddd50429efce706736614eb6da2db8719ee4
totosuki <e231kari@gmail.com>
2025-03-20 00:07:49 +0900

    Ruffになおしてもらった

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
-import os
 import datetime
+import os
 import random
+
 import pandas as pd
+
 
 class Data:
     def __init__(self):
@@ -9,18 +11,22 @@ class Data:
             self.reset()
         else:
             self.df = pd.read_csv("data.csv")
-    
+
     def reset(self):
-        self.df = pd.DataFrame(columns = ["id", "sentence", "created_at"])
-        self.df.to_csv('data.csv', index=False)
-    
+        self.df = pd.DataFrame(columns=["id", "sentence", "created_at"])
+        self.df.to_csv("data.csv", index=False)
+
     def add(self, sentence):
         self.df = pd.read_csv("data.csv")
-        new_row = pd.DataFrame([{
-            "id" : self.df.shape[0] + 1,
-            "sentence" : sentence,
-            "created_at" : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        }])
+        new_row = pd.DataFrame(
+            [
+                {
+                    "id": self.df.shape[0] + 1,
+                    "sentence": sentence,
+                    "created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            ]
+        )
         self.df = pd.concat([self.df, new_row], ignore_index=True)
         self.df.to_csv("data.csv", index=False)
 
