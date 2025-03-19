a7088b71f0b04ef887e74db190a47788e2402bb4
toripppppy <lundy11@icloud.com>
2025-03-20 00:13:56 +0900

    適当なフロントができた

---
+++ front/script.js
@@ -0,0 +1,56 @@
+function showCard() {
+  document.body.style.background = "white";
+  document.getElementById("initialMessage").classList.add("hidden");
+  document.getElementById("praiseMessage").classList.add("show");
+  document.body.onclick = null; // 一度クリックしたら無効にする
+  startParticles();
+}
+
+function startParticles() {
+  const canvas = document.getElementById("particles");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  let particlesArray = [];
+
+  class Particle {
+    constructor() {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+      this.size = Math.random() * 5 + 2;
+      this.speedX = (Math.random() - 0.5) * 2;
+      this.speedY = (Math.random() - 0.5) * 2;
+    }
+    update() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      if (this.size > 0.2) this.size -= 0.02;
+    }
+    draw() {
+      ctx.fillStyle = "rgb(255, 191, 17)";
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+  function initParticles() {
+    particlesArray = [];
+    for (let i = 0; i < 100; i++) {
+      particlesArray.push(new Particle());
+    }
+  }
+
+  function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particlesArray.length; i++) {
+      particlesArray[i].update();
+      particlesArray[i].draw();
+    }
+    requestAnimationFrame(animateParticles);
+  }
+
+  initParticles();
+  animateParticles();
+}
