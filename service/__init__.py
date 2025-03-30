6a5ed4dcef09e0bfed383c001864ca87169bdc14
toripppppy <lundy11@icloud.com>
2025-03-30 14:32:22 +0900

    RuffのF401エラーの回避

--- service/__init__.py
+++ service/__init__.py
@@ -1,3 +1,5 @@
 from .data_service import DataService
 from .home_service import HomeService
 from .login_record_service import LoginRecordService
+
+__all__ = ["DataService", "HomeService", "LoginRecordService"]
