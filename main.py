fae7800bbf694d9fbebde31dc8ba30d405bf8691
totosuki <e231kari@gmail.com>
2025-04-07 12:18:02 +0900

    型チェッカーの警告に対処

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from dataclasses import asdict
+from typing import Annotated
 
 import uvicorn
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -24,7 +25,7 @@ login_record_service = LoginRecordService(login_record_dao)
 
 
 class PostHomeRequest(BaseModel):
-    sentence: constr(strip_whitespace=True, min_length=1, max_length=20)
+    sentence: Annotated[str, constr(strip_whitespace=True, min_length=1, max_length=20)]
 
 
 # ほめ言葉を一つ返す
