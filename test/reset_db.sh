50053f43d6a99b17ed04fe249caec5482ddc2af4
toripppppy <lundy11@icloud.com>
2025-04-14 17:51:40 +0900

    test/reset_db.sh を修正

--- test/reset_db.sh
+++ test/reset_db.sh
@@ -4,4 +4,4 @@ touch db/homes.csv
 touch db/login_records.csv
 echo "id,sentence,is_used,created_at" >> db/homes.csv
 echo "1,すごいね！,False,2025-04-07 11:20:30" >> db/homes.csv
-echo "ip,home_id,created_at,has_posted" >> db/login_records.csv
+echo "hash,ip,home_id,created_at,has_posted" >> db/login_records.csv
